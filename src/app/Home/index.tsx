887819916ac50be8a7b5a703b2529cef43799306
Frank Osorio <frankosorio4@gmail.com>
2026-01-15 18:40:31 -0300

    chore: initial commit

---
+++ src/app/Home/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native"
+import { styles } from "./styles"
+
+export default function Home() {
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello Wolrd!</Text>
+    </View>
+  )
+}
+
