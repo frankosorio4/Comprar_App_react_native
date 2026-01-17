e8e7b1660ac7317b390ac771675f83e30aae29b4
Frank Osorio <frankosorio4@gmail.com>
2026-01-16 21:30:34 -0300

    feat: component Button added

---
+++ src/Components/Button/index.tsx
@@ -0,0 +1,12 @@
+import { TouchableOpacity, Text } from "react-native";
+import { styles } from "./styles"
+
+export function Button() {
+
+    return (
+        <TouchableOpacity style={styles.container} activeOpacity={0.8}>
+            <Text style={styles.title}>Adiconar</Text>
+        </TouchableOpacity>
+    )
+}
+
