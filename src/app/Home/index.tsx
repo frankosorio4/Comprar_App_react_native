7da5a055ab54fc58b9b6295c8adbcfddb160f190
Frank Osorio <frankosorio4@gmail.com>
2026-01-18 15:45:11 -0300

    feat: input componet

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -1,12 +1,15 @@
 import { View, Image } from "react-native"
 import { styles } from "./styles"
 import { Button } from "@/Components/Button"
+import { Input } from "@/Components/Input"
 
 export default function Home() {
 
   return (
     <View style={styles.container}>
       <Image style={styles.logo} source={require("@/assets/logo.png")} />
+
+      <Input placeholder="O que vai comprar?" />
 
       <Button title="Entrar" />
       {/* <Button
