037bffbc9795a9b30d58e02554cac51955ee2a05
Frank Osorio <frankosorio4@gmail.com>
2026-01-20 16:31:12 -0300

    feat: lucide library icons added

--- src/Components/Filter/index.tsx
+++ src/Components/Filter/index.tsx
@@ -1,4 +1,5 @@
 import { TouchableOpacity, TouchableOpacityProps, Text } from "react-native";
+import { CircleCheck } from "lucide-react-native"
 
 import { styles } from "./styles";
 import { FilterStatus } from "@/Types/FilterStatus";
@@ -14,7 +15,9 @@ export function Filter({ status, isActive, ...rest }: Props) {
         <TouchableOpacity
             style={styles.container}
             activeOpacity={0.8}
-            {...rest}>
+            {...rest}
+        >
+            <CircleCheck size={18} color="#000" />
             <Text style={[
                 { ...styles.title },
                 { opacity: isActive ? 1 : 0.5 }
